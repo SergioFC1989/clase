add62c750ae104cf787cf9c1351a4008568802fa
SergioFC1989 <sergiofc@hotmail.es>
2025-07-22 15:44:04 +0200

    refactor: refactor code structure for improved readability and maintainability

--- src/lib/components/root-layout/RootLayout.tsx
+++ src/lib/components/root-layout/RootLayout.tsx
@@ -4,7 +4,7 @@ import "@/lib/styles/style.css";
 import Image from "next/image";
 import { ReactNode } from "react";
 
-import Logo from "@/lib/assets/logo.png";
+import NameLogo from "@/lib/assets/name-logo.png";
 import { AppProvider } from "@/lib/context/app-context";
 import Theme from "@/lib/themes/theme";
 
@@ -12,10 +12,12 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <AppProvider>
       <Theme>
-        <header className="fixed top-0 left-0 w-full bg-primary-color shadow-2xl z-50 px-2 print:hidden">
-          <Image src={Logo} alt="Logo" width={200} height={200} />
+        <header className="fixed top-0 left-0 w-full bg-primary-color shadow-gray-500 shadow-lg z-50 px-2 print:hidden">
+          <div className="flex flex-row gap-6 py-2 px-8">
+            <Image src={NameLogo} alt="NameLogo" width={100} height={100} />
+          </div>
         </header>
-        <div className="w-full h-screen overflow-auto print:h-auto flex flex-col px-1 lg:px-10 pt-28 print:pt-10 pb-10 bg-white">
+        <div className="w-full h-screen overflow-auto print:h-auto flex flex-col px-1 lg:px-10 pt-20 print:pt-10 pb-8 bg-white">
           {children}
         </div>
       </Theme>
