a95d964c44bc4d4b12c48d1c1dff5ccb9efd9915
SergioFC1989 <sergiofc@hotmail.es>
2025-07-28 18:23:17 +0200

    feat: add ESM support with __dirname emulation in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+// 👇 Emular __dirname en ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+};
 
 export default nextConfig;
