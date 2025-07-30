5f25cb57b3d597f1f95f338a035ccb407d7a6ab8
SergioFC1989 <sergiofc@hotmail.es>
2025-07-31 01:16:26 +0200

    fix: deleted dashboard module

--- src/modules/dashboard/Dashboard.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import EducationalCenter from "../educational-center/EducationalCenter";
-
-const Dashboard = () => {
-  return <EducationalCenter />;
-};
-
-export default Dashboard;
